77e5ce023c9fa53342b210e019e1c8c807fd6c39
Albert <albert.araque.casaus@gmail.com>
2020-05-28 19:17:32 +0200

    Arreglo de fallos

--- src/dao/SessionFactoryUtil.java
+++ src/dao/SessionFactoryUtil.java
@@ -2,6 +2,10 @@ package dao;
 
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import view.Main;
 /**
  * Clase para obtener la sesion en la base de datos
  * 
@@ -9,7 +13,7 @@ import org.hibernate.cfg.Configuration;
  *
  */
 public class SessionFactoryUtil {
-	private static final SessionFactory sessionFactory;
+	private static SessionFactory sessionFactory = null;
 
 	static {
 		try {
@@ -17,13 +21,22 @@ public class SessionFactoryUtil {
 			sessionFactory = new Configuration().configure().buildSessionFactory();
 
 		} catch (Throwable ex) {
-			// Make sure you log the exception, as it might be swallowed
-			System.err.println("Initial SessionFactory creation failed." + ex);
-			throw new ExceptionInInitializerError(ex);
+			showAlert();
 		}
+	}
+
+	private static void showAlert() {
+		Alert errorAlert = new Alert(AlertType.ERROR);
+		errorAlert.setTitle("Base de datos en uso");
+		errorAlert.setHeaderText("No ha sido posible cargar la base de datos");
+		errorAlert.setContentText("No ha sido posible cargar la base de datos debido a que ya esta en uso, es posible que haya abierto el programa dos veces.");
+		errorAlert.showAndWait();
+		Main.getStage().hide();
 	}
 
 	public static SessionFactory getSessionFactory() {
 		return sessionFactory;
 	}
+	
+	
 }
